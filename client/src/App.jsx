48f756544ff73caa2b97dace6626cb27255015ff
unknown <carlaa_58@hotmail.com>
2024-05-29 14:00:03 +1200

    SideBar add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Cookies from 'universal-cookie';
 
 import { ChannelListContainer, ChannelContainer } from './components';
 
+import './App.css';
+
 const apiKey = 'tek6cgrz6922';
 
 const client = StreamChat.getInstance(apiKey);
